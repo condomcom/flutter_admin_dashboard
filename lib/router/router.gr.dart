0b9f486651dc4801c4e0a8cdd0563b0ce1ebf7c1
Stanislav Ilin <youngfrezyx@gmail.com>
2021-10-22 02:28:13 +0500

    Make build

--- lib/router/router.gr.dart
+++ lib/router/router.gr.dart
@@ -11,6 +11,8 @@
 import 'package:auto_route/auto_route.dart' as _i4;
 import 'package:flutter/material.dart' as _i5;
 
+import '../models/conference/conference.dart' as _i7;
+import '../models/user/user.dart' as _i6;
 import '../screens/conference/conference.dart' as _i3;
 import '../screens/main/main_screen.dart' as _i1;
 import '../screens/user/user.dart' as _i2;
@@ -26,12 +28,19 @@ class AppRouter extends _i4.RootStackRouter {
           routeData: routeData, child: _i1.MainScreen());
     },
     UserEditScreenRoute.name: (routeData) {
+      final args = routeData.argsAs<UserEditScreenRouteArgs>(
+          orElse: () => const UserEditScreenRouteArgs());
       return _i4.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i2.UserEditScreen());
+          routeData: routeData,
+          child: _i2.UserEditScreen(key: args.key, user: args.user));
     },
     ConferenceEditScreenRoute.name: (routeData) {
+      final args = routeData.argsAs<ConferenceEditScreenRouteArgs>(
+          orElse: () => const ConferenceEditScreenRouteArgs());
       return _i4.MaterialPageX<dynamic>(
-          routeData: routeData, child: const _i3.ConferenceEditScreen());
+          routeData: routeData,
+          child: _i3.ConferenceEditScreen(
+              key: args.key, conference: args.conference));
     }
   };
 
@@ -52,16 +61,39 @@ class MainScreenRoute extends _i4.PageRouteInfo<void> {
 }
 
 /// generated route for [_i2.UserEditScreen]
-class UserEditScreenRoute extends _i4.PageRouteInfo<void> {
-  const UserEditScreenRoute() : super(name, path: '/user-edit-screen');
+class UserEditScreenRoute extends _i4.PageRouteInfo<UserEditScreenRouteArgs> {
+  UserEditScreenRoute({_i5.Key? key, _i6.User? user})
+      : super(name,
+            path: '/user-edit-screen',
+            args: UserEditScreenRouteArgs(key: key, user: user));
 
   static const String name = 'UserEditScreenRoute';
 }
 
+class UserEditScreenRouteArgs {
+  const UserEditScreenRouteArgs({this.key, this.user});
+
+  final _i5.Key? key;
+
+  final _i6.User? user;
+}
+
 /// generated route for [_i3.ConferenceEditScreen]
-class ConferenceEditScreenRoute extends _i4.PageRouteInfo<void> {
-  const ConferenceEditScreenRoute()
-      : super(name, path: '/conference-edit-screen');
+class ConferenceEditScreenRoute
+    extends _i4.PageRouteInfo<ConferenceEditScreenRouteArgs> {
+  ConferenceEditScreenRoute({_i5.Key? key, _i7.Conference? conference})
+      : super(name,
+            path: '/conference-edit-screen',
+            args: ConferenceEditScreenRouteArgs(
+                key: key, conference: conference));
 
   static const String name = 'ConferenceEditScreenRoute';
+}
+
+class ConferenceEditScreenRouteArgs {
+  const ConferenceEditScreenRouteArgs({this.key, this.conference});
+
+  final _i5.Key? key;
+
+  final _i7.Conference? conference;
 }
